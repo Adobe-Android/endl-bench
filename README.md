9b70397e910135a5a6ece0d57faa4e2b5fa1dd8f
Adobe-Android <tngamerboy@gmail.com>
2020-04-25 19:45:13 -0500

    initial project commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# endl_bench
+
+## What is this?
+A simple comparison between using std::endl and manually adding a newline character.
+
+## Why?
+It is well known in the C++ community that std::endl has a problem. That problem is that it is unnecessarily slow.
+It does two things every time you call std::endl.
+```
+std::cout << "hello" << std::endl;
+```
+You do the equivalent of this which is fairly apparent, but you also flush the buffer.
+```
+std::cout << "hello" << "\n";
+```
+
+## What kind of performance hit can I expect?
+It will wildly vary based on your use case how expensive this operation is and whether you should have used it in the first place. Here are my results of this trivial benchmark.
+
+```
+It took say_hello 190 ms
+It took say_hello_with_flush 348 ms
+```
+
+## References
+My favorite article on the subject:
+https://accu.org/index.php/journals/2619
---
+++ endl.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <chrono>
+
+int say_hello_with_flush()
+{
+    std::cout << "hello" << std::endl;
+    return 0;
+}
+
+int say_hello()
+{
+    std::cout << "hello\n";
+    return 0;
+}
+
+int main()
+{
+    auto time_start = std::chrono::high_resolution_clock::now();
+    for (size_t i = 0; i < 1'000; i++)
+    {
+        say_hello();
+    }
+
+    auto time_end = std::chrono::high_resolution_clock::now();
+    auto duration1 = std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count();
+
+    time_start = std::chrono::high_resolution_clock::now();
+    for (size_t i = 0; i < 1'000; i++)
+    {
+        say_hello_with_flush();
+    }
+
+    time_end = std::chrono::high_resolution_clock::now();
+    auto duration2 = std::chrono::duration_cast<std::chrono::milliseconds>(time_end - time_start).count();
+
+    std::cout << "It took say_hello " << duration1 << " ms\n";
+    std::cout << "It took say_hello_with_flush " << duration2 << " ms\n";
+}
